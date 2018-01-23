29a7ecb8d894ac06789cde4906f54a83a518097c
lumi <luj@spsp-it.com>
2018-01-23 15:49:06 +0800

    临时提交

--- ArticleSpider/spiders/zhihu_sel.py
+++ ArticleSpider/spiders/zhihu_sel.py
@@ -1,10 +1,15 @@
 # -*- coding:utf-8 -*-
+import re
 
 import scrapy
 
 import urlparse
 
 import os
+
+from scrapy.loader import ItemLoader
+
+from ArticleSpider.items import ZhihuQuestionItem
 
 
 class ZhihuSpider(scrapy.Spider):
@@ -32,12 +37,47 @@ class ZhihuSpider(scrapy.Spider):
         """
         all_urls = response.css('a::attr(href)').extract()
         all_urls = [urlparse.urljoin(response.url, url) for url in all_urls]
+        all_urls = filter(lambda x: True if x.startswith('https') else False, all_urls)
         for url in all_urls:
-            pass
+            # print url
+            match_obj = re.match("(.*zhihu.com/question/(\d+))(/|$).*", url)
+            if match_obj:
+                request_url = match_obj.group(1)
+                question_id = match_obj.group(2)
+
+                yield scrapy.Request(url=request_url, meta={'question_id': question_id}, headers=self.headers, callback=self.parse_question)
 
     def parse_question(self, response):
         # 处理question页面， 从页面中提取出具体的question item
-        pass
+        item_loader = ItemLoader(item=ZhihuQuestionItem(), response=response)
+        if 'QuestionHeader-title' in response.text:
+            # 处理新版本
+            item_loader.add_css('title', 'h1.QuestionHeader-title::text')
+            item_loader.add_css('content', '.QuestionHeader-detail')
+            item_loader.add_value('url', response.url)
+            item_loader.add_value('zhihu_id', int(response.meta.get('question_id')))
+            item_loader.add_css('answer_num', '.QuestionMainAction a::text')
+            item_loader.add_css('comments_num', '.QuestionHeader-Comment button::text')
+            # button.NumberBoard-item > div:nth-child(1) > strong:nth-child(2)
+            item_loader.add_css('watch_user_num', '[data-reactid="104"]::text')
+            item_loader.add_css('click_num', '[data-reactid="108"]::text')
+            item_loader.add_css('topics', '.QuestionHeader-tags .Popover::text')
+
+            question_item = item_loader.load_item()
+        else:
+            # 处理老版本
+            item_loader.add_css('title', 'h1.QuestionHeader-title::text')
+            item_loader.add_css('content', '.QuestionHeader-detail')
+            item_loader.add_value('url', response.url)
+            item_loader.add_value('zhihu_id', int(response.meta.get('question_id')))
+            item_loader.add_css('answer_num', '.QuestionMainAction a::text')
+            item_loader.add_css('comments_num', '.QuestionHeader-Comment button::text')
+            # button.NumberBoard-item > div:nth-child(1) > strong:nth-child(2)
+            item_loader.add_css('watch_user_num', '[data-reactid="104"]::text')
+            item_loader.add_css('click_num', '[data-reactid="108"]::text')
+            item_loader.add_css('topics', '.QuestionHeader-tags .Popover::text')
+
+            question_item = item_loader.load_item()
 
     def parse_answer(self, reponse):
         pass
